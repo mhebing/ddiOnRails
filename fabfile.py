5f9d2998c4ad1f3983a668ded7654e4f39172709
afuetterer <fuetterh@posteo.de>
2018-12-08 16:31:17 +0100

    Add fabric tasks

--- fabfile.py
+++ fabfile.py
@@ -1,60 +1,30 @@
 import os
-import shutil
 from pprint import pprint
-
-from django.contrib.auth import get_user_model
-
-from fabric.colors import cyan, green
-from fabric.context_managers import cd, hide
-from fabric.contrib import django
-from fabric.decorators import roles, task
-from fabric.operations import env, local, prompt, run, sudo
-from fabric.tasks import execute
-from fabric.utils import puts
-
-env.project_name = 'ddionrails'
+from fabric import task
+from invoke import Collection
 
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-print(BASE_DIR)
-django.settings_module('settings.development')
+from pathlib import Path
 
 
-import django # isort:skip
-django.setup()
+# os.environ.setdefault("LC_ALL", "C.UTF-8")
+# os.environ.setdefault("LANG", "C.UTF-8")
 
+# os.environ['LC_ALL'] = 'C.UTF-8'
+# os.environ['LC_CTYPE'] = 'C.UTF-8'
+# os.environ['LANG'] = 'C.UTF-8'
 
-ADMIN_EMAIL_ADDRESS = 'admin@example.com'
+# os.environ['LC_ALL'] = 'en_US.UTF-8'
 
-# env.project_name = '{{ project_name }}'
-env.project_root_dir = os.path.dirname(os.path.abspath(__file__))
-
-env.project_src_dir = env.project_root_dir
-
-env.hosts = ['localhost']
-
-
-def pipenv_run(command):
-    """
-    Run a "pipenv run" command on the server.
-    """
-    with cd(env.project_src_dir):
-        run('pipenv run {pipenv_command}'.format(pipenv_command=command, **env))
-
-
-def pipenv_run_local(command):
-    """
-    Run a "pipenv run" command locally.
-    """
-    local('pipenv run {pipenv_command}'.format(pipenv_command=command, **env))
+# pprint(os.environ["LC_ALL"])
+# pprint(os.environ["LC_CTYPE"])
+# pprint(os.environ["LANG"])
 
 
 @task
-def migrate():
-    """
-    Run DB migrations.
-    """
-    pipenv_run_local('python manage.py migrate')
+def npm(c):
+    """ Install all frontend dependencies with npm """
+    c.run("npm install")
 
 @task
 def python(c):
@@ -64,27 +34,73 @@ def python(c):
     c.run("which python3")
 
 
+@task
+def venv(c):
+    """ Setup the python virtual environment for the project """
+    pip = Path.home() / ".envs/ddionrails/bin/pip"
+    if not pip.exists():
+        print("setting up new virtual environment")
+        # c.run("/usr/bin/python3.6 -m venv ~/.envs/ddionrails")
+        # c.run("source ~/.envs/ddionrails/bin/activate")
+        # c.run("~/.envs/ddionrails/bin/pip3 install -U pip wheel pipenv")
+        # c.run("~/.envs/ddionrails/bin/pipenv")
+
+    c.run("source ~/.envs/ddionrails/bin/activate")
+    print("updating requirements")
+    print("pipenv install --dev")
+
+# os.environ['LC_ALL'] = 'C.UTF-8'
+# os.environ['LC_CTYPE'] = 'C.UTF-8'
+
 
 @task
-def pipenv():
-    pprint(env)
+def pipenv(c):
+    """ pipenv """
+    c.run("source ~/.envs/ddionrails/bin/activate")
+    # c.run("~/.envs/ddionrails/bin/pip -V")
+    # c.run("~/.envs/ddionrails/bin/pipenv", replace_env=False)
+    # c.run("env", replace_env=False)
+    # with c.prefix('LC_ALL=C.UTF-8'):
+    #     c.run('echo $LC_ALL')  # prints "production"
 
+    # pipenv = "~/.envs/ddionrails/bin/pipenv"
 
+    # c.run(pipenv, env={"LC_ALL": "C.UTF-8"})
 
-    run("which python")
-    # local("pipenv")
+    c.run(
+        "~/.envs/ddionrails/bin/pipenv install --dev",
+        # env={"LC_ALL": "C.UTF-8", "LANG": "C.UTF-8"},
+        replace_env=False, echo=True
+    )
+    c.run("~/.envs/ddionrails/bin/pipenv graph", replace_env=False)
+
+@task
+def piplist(c):
+    c.run("~/.envs/ddionrails/bin/pipenv graph", replace_env=False, echo=True)
 
 
 @task
-def pipenv_requirements():
-    """
-    Update the requirements using global pipenv.
-    """
-    with cd(env.project_src_dir):
-        run('pipenv install --dev')
-
+def django(c):
+    c.run("~/.envs/ddionrails/bin/pipenv run django-admin", replace_env=False, echo=True)
 
 @task
-def copy_secrets_file():
-    """ Copy the secrets.json.example and rename it to secrets.json """
-    shutil.copyfile("secrets.json.example", "secrets.json")
+def migrate(c):
+    # with c.prefix('~/.envs/ddionrails/bin/pipenv shell'):
+    c.run("~/.envs/ddionrails/bin/pipenv run ./manage.py migrate", replace_env=False, echo=True)
+
+@task
+def server(c):
+    # with c.prefix('~/.envs/ddionrails/bin/pipenv shell'):
+    c.run("~/.envs/ddionrails/bin/pipenv run ./manage.py runserver", replace_env=False, echo=True)
+
+
+
+
+
+# inner = Collection('setup', venv, pipenv)
+# front = Collection('frontend', npm)
+# # inner.configure({'conflicted': 'default value'})
+#
+# # Our project's root namespace.
+# ns = Collection(inner, front)
+# # ns.configure({'conflicted': 'override value'})
